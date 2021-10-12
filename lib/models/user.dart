46934c9e16e5ababa984656b637bc4738ae9e9b3
Ajidan Jegatheeswaran <63339318+Ajidan-Jegatheeswaran@users.noreply.github.com>
2021-10-12 12:46:17 +0200

    Create user.dart
    I made a model named user. It already have following variables: id, username, password, url

---
+++ lib/models/user.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class User {
+  final int id;
+  final String username;
+  final String password;
+  final String url;
+
+  User(
+      {@required required this.id,
+      @required required this.username,
+      @required required this.password,
+      @required required this.url});
+
+  void log_in_nesa() {
+    
+  }
+}
